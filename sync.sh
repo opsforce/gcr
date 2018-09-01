3558d019a26b4207871336391d3b7448961d3fea
mritd <mritd1234@gmail.com>
2018-09-01 21:26:16 +0800

    chore(ci): disable debug
    disable debug
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- sync.sh
+++ sync.sh
@@ -4,7 +4,7 @@ set -e
 
 wget https://github.com/mritd/gcrsync/releases/download/v1.0.0/gcrsync_linux_amd64 -O gcrsync
 chmod +x gcrsync
-./gcrsync --debug --user ${DOCKER_USER} --password ${DOCKER_PASSWORD}
+./gcrsync --user ${DOCKER_USER} --password ${DOCKER_PASSWORD}
 
 rm -f gcrsync
 
