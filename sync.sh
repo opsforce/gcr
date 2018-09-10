f22b6ea6c4a79a1554f5e349d09d3112ae3efcbf
mritd <mritd1234@gmail.com>
2018-09-10 11:02:22 +0800

    feat(gcrsync): update 1.1.4
    update 1.1.4
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- sync.sh
+++ sync.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-GCRSYNC_VERSION=v1.1.3
+GCRSYNC_VERSION=v1.1.4
 
 wget -q https://github.com/mritd/gcrsync/releases/download/${GCRSYNC_VERSION}/gcrsync_linux_amd64 -O gcrsync
 chmod +x gcrsync
-./gcrsync --querylimit 100 --processlimit 10 --user ${DOCKER_USER} --password ${DOCKER_PASSWORD}
+./gcrsync --querylimit 20 --processlimit 10 --user ${DOCKER_USER} --password ${DOCKER_PASSWORD}
 
 rm -f gcrsync
 
-#git add .
-#git commit -m "Travis CI Auto Synchronized."
-#git push --force https://$GITHUB_TOKEN@github.com/mritd/gcr.git master
+git add .
+git commit -m "Travis CI Auto Synchronized."
+git push --force https://$GITHUB_TOKEN@github.com/mritd/gcr.git master
